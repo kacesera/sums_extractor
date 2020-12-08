e5df9df7b5fd7756b2fa4fbd4989085a0c41a3d3
KC Thomas <thisiskcthomas@gmail.com>
2020-12-08 14:22:32 +0000

    #extract method returns array with sums_string and sum of that string

--- lib/sums_extractor.rb
+++ lib/sums_extractor.rb
@@ -1,7 +1,24 @@
 class SumsExtractor
 
   def self.extract(sums_string) 
-    [sums_string]
+    extracted_sums = [sums_string]
+
+    split_string = sums_string.split(" ")
+    first_num = split_string[0].to_i
+    second_num = split_string[2].to_i
+    operator = split_string[1]
+
+    if operator == "+"
+      extracted_sums << (first_num + second_num)
+    elsif operator == "-"
+      extracted_sums << (first_num - second_num)
+    elsif operator == "*"
+      extracted_sums << (first_num * second_num)
+    elsif operator == "/"
+      extracted_sums << (first_num / second_num)
+    end
+
+    extracted_sums
   end
 
 end
--- spec/sums_extractor_spec.rb
+++ spec/sums_extractor_spec.rb
@@ -3,8 +3,16 @@ require 'sums_extractor'
 describe SumsExtractor do
 
   describe "#extract_sum" do
+
+    before :each do
+      @extracted_sum = SumsExtractor.extract("1 + 1")
+    end
     it "returns the sums string in an array" do
-      expect(SumsExtractor.extract("1 + 1")[0]).to eq "1 + 1"
+      expect(@extracted_sum[0]).to eq "1 + 1"
+    end
+
+    it "returns the sum of the sums string in an array" do
+      expect(@extracted_sum[1]).to eq 2
     end
   end
 end
